01d326179c500e338f0babb90feb8e5f9f76735b
unknown <asiac00@gmail.com>
2018-01-09 21:53:11 +0100

    update to do

---
+++ 16.2.2 Zadanie/src/components/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from '../style/Todo.css';
+
+
+
+
+export default Todo; 
--- 16.2.2 Zadanie/webpack.config.js
+++ 16.2.2 Zadanie/webpack.config.js
@@ -1,8 +1,38 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
+
+
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+	'react-hot-loader/patch',
+	'./src/index.js'
+],
         output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
@@ -33,6 +63,10 @@ module.exports = {
   template: 'src/index.html',
   filename: 'index.html',
   inject: 'body'
+}),
+new webpack.optimize.UglifyJsPlugin(),
+new OptimizeJsPlugin({
+  sourceMap: false
 })
 ]
   
