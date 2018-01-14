e51e0508726df24d9995c7c61296a84cc9c28963
unknown <asiac00@gmail.com>
2018-01-14 13:18:10 +0100

    minor fix

--- 16.2.2 Zadanie/src/components/TodoForm.js
+++ 16.2.2 Zadanie/src/components/TodoForm.js
@@ -6,20 +6,20 @@ class TodoForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            taskName: ''
+            taskTitle: ''
 		};
-	this.changeTaskName = this.changeTaskName.bind(this);
+	this.changeTaskTitle = this.changeTaskTitle.bind(this);
 }
-	changeTaskName(event) {
+	changeTaskTitle(event) {
         this.setState({
-            taskName: event.target.value
+            taskTitle: event.target.value
         });
 }
 	render() {
 		return (
 			<form className={style.TodoForm}>
-					<input className={style.TodoInput} type={"text"} value={this.state.taskName}  onChange={this.changeTaskName} placeholder = "Add new task here"/>
-					<button className={style.AddButton} type={"button"} onClick={() => this.props.addTodo(this.state.taskName)}>Add</button>
+					<input className={style.TodoInput} type={"text"} value={this.state.taskTitle}  onChange={this.changeTaskTitle} placeholder = "Add new task here"/>
+					<button className={style.AddButton} type={"button"} onClick={() => this.props.addTodo(this.state.taskTitle)}>Add</button>
 			</form>
 		)
 	};
